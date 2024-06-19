ba763b35618f13601de4a77d03581cce5f2f9f4f
Soso_SantosS2 <sosodasruas@gmail.com>
2024-06-19 13:22:19 -0300

    adicionando arquivos

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+  
+    stroke("blue")
+    background("white"); 
+    fill("red")
+  
+    // console.log(mouseIsPressed)
+    rect(mouseX,mouseY,100,150)    
+  
+  }
